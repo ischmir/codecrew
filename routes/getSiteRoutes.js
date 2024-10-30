5c526545238c268d66e1a7b586442a78793e1e86
Mai Jockwich <mmjockwich@gmail.com>
2024-10-30 19:12:17 +0100

    Fixed crash

--- routes/getSiteRoutes.js
+++ routes/getSiteRoutes.js
@@ -6,7 +6,7 @@ module.exports = function (app) {
 	app.get('/dashboard', dashboardC.dashboard);
 	app.get('/login', loginC.login);
 	app.get('/signup', loginC.signup);
-	app.get('/settings', loginC.settings);
+	app.get('/settings', settingsC.settings);
 
 	// app.get("/UserSetting", settingsC.userSetting);
 	// app.get("/AdminSetting", siteC.singleUser)
