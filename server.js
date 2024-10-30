3fae1a51de45d2c628695be5b709def8582e7390
Mai Jockwich <mmjockwich@gmail.com>
2024-10-30 18:58:39 +0100

    settings
    settings + ryddet op i filerne, blandt andet slettet node_Moduls mappen

--- server.js
+++ server.js
@@ -3,53 +3,50 @@ const exphbs = require('express-handlebars');
 const path = require('path');
 
 const app = express();
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.engine('hbs', exphbs.engine({ 
-    defaultLayout: 'main',
-    extname: '.hbs',
-    partialsDir: path.join(__dirname, 'views/partials')
-}));
+app.engine(
+	'hbs',
+	exphbs.engine({
+		defaultLayout: 'main',
+		extname: '.hbs',
+		partialsDir: path.join(__dirname, 'views/partials'),
+	})
+);
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 //Example data
 const data = {
-    stack: [
-        {id: 0[
-            {name: "sandra-nginx"},
-            {author: "codecrew"},
-            {creationDate: "29.10.2024"},
-            {status: 1}
-        ]},
-        {id: 1[
-            {name: "timm-nginx"},
-            {author: "codecrew"},
-            {creationDate: "20.10.2024"},
-            {status: 1}
-        ]}
-    ],
-    title: "Dashboard",
-    isAdmin: true
-}
+	stack: [
+		{ id: (0)[({ name: 'sandra-nginx' }, { author: 'codecrew' }, { creationDate: '29.10.2024' }, { status: 1 })] },
+		{ id: (1)[({ name: 'timm-nginx' }, { author: 'codecrew' }, { creationDate: '20.10.2024' }, { status: 1 })] },
+	],
+	title: 'Dashboard',
+	isAdmin: true,
+};
 
 app.get('/dashboard', (req, res) => {
-    res.render('dashboard', data);
+	res.render('dashboard', data);
 });
 
-app.get("/nav", (req, res) => {
-    res.render("nav", {
-        isAdmin: true
-    })
+app.get('/nav', (req, res) => {
+	res.render('nav', {
+		isAdmin: true,
+	});
 });
 
-app.get("/login", (req, res) => {
-    res.render("index")
+app.get('/login', (req, res) => {
+	res.render('index');
 });
 
-app.get("/signup", (req, res) => {
-    res.render("signup")
+app.get('/signup', (req, res) => {
+	res.render('signup');
+});
+
+app.get('/settings', (req, res) => {
+	res.render('settings');
 });
 
 const PORT = process.env.PORT || 3333;
