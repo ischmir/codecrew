21df6fea48508baf08bb44d772d498b62cba06cb
Sandrastorm <sandrastorm8@hotmail.com>
2024-10-31 13:50:45 +0100

    nyt design 31/10

--- public/script/script.js
+++ public/script/script.js
@@ -1,11 +1,1 @@
 
-/*Sign up*/ 
-
-const inputField = document.getElementById('myInput');
-
-inputField.addEventListener('input', (event) => {
-    // Håndter input som ønsket
-    console.log(event.target.value); // Vis værdien i konsollen
-});
-
-
