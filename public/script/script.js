73c51a8c369c2e5faedc86a682b1b6e5a91f718f
Sandrastorm <sandrastorm8@hotmail.com>
2024-10-31 08:00:24 +0100

    sidste update 30/10

---
+++ public/script/script.js
@@ -0,0 +1,11 @@
+
+/*Sign up*/ 
+
+const inputField = document.getElementById('myInput');
+
+inputField.addEventListener('input', (event) => {
+    // Håndter input som ønsket
+    console.log(event.target.value); // Vis værdien i konsollen
+});
+
+
--- server.js
+++ server.js
@@ -54,3 +54,4 @@ app.get("/signup", (req, res) => {
 
 const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
