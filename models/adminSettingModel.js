88bcb861ac5b0f9f04af16a3cbc96d172e5fdd80
Marcus <marc3277@edu.zealand.dk>
2024-11-15 16:06:00 +0100

    fixed db down for admin_user_settings

--- models/adminSettingModel.js
+++ models/adminSettingModel.js
@@ -1,18 +1,28 @@
 const db = require("../config/db");
 
 exports.adminSettingsUpgradeUser = async function() {    
-    const [roles, fields] = await db.query("SELECT roleId, roleName FROM Roles");
-    const [names] = await db.query("SELECT userId, firstName, lastName FROM Users")
-    console.log(names);
-    
-    const data = {
-        title: "Admin settings",
-        roles, 
-        names
-    }
-        console.log(data);
+    try {
+        const [roles, fields] = await db.query("SELECT roleId, roleName FROM Roles");
+        const [names] = await db.query("SELECT userId, firstName, lastName FROM Users")
+        console.log(names);
         
-    return data;
+        const data = {
+            title: "Admin settings",
+            roles, 
+            names
+        }
+            console.log(data);
+            
+        return data;
+    }
+    catch (err) {
+        const data = {
+            title: "Admin settings"
+        }
+        console.log(err);
+        
+        return data
+    }
 }
 exports.updateStackLimit = async function (newStackLimit, accessLevel) {
     const [result] = await db.execute("UPDATE Roles SET stackLimit = ? WHERE accessLevel = ?", [newStackLimit, accessLevel]);
