06c8d599af27a6fd633279cd24738c2488b35391
Mai Jockwich <mmjockwich@gmail.com>
2024-11-20 16:27:29 +0100

    Teams stuff

--- public/script/teams.js
+++ public/script/teams.js
@@ -1,15 +1,33 @@
-const fileInput = document.getElementById('csvFile');
-fileInput.addEventListener('change', function () {
-	const file = fileInput.files[0];
-	if (file && file.type === 'text/csv') {
-		alert('CSV file has been selected: ' + file.name);
-		handleCsvFile(file);
-	} else {
-		alert('Please select a CSV file.');
-	}
-});
+console.log('Script is running!');
 
-function handleCsvFile(file) {
-	console.log(file.name);
-	// Implement file handling logic here
-}
+// const csvDropzone = document.getElementById('csv-dropzone');
+// console.log(csvDropzone ? 'Dropzone found!' : 'Dropzone not found!');
+// csvDropzone.addEventListener('dragover', function (event) {
+// 	event.preventDefault();
+// 	csvDropzone.classList.add('dragover');
+
+// 	console.log('Dragover event triggered!');
+// });
+// csvDropzone.addEventListener('dragleave', function (event) {
+// 	event.preventDefault();
+// 	csvDropzone.classList.remove('dragover');
+
+// 	console.log('Drop event triggered!');
+// 	console.log('Files:', event.dataTransfer.files);
+// });
+// csvDropzone.addEventListener('drop', function (event) {
+// 	event.preventDefault();
+// 	csvDropzone.classList.remove('dragover');
+// 	const csvFile = event.dataTransfer.files[0];
+// 	if (csvFile && (csvFile.type === 'text/csv' || csvFile.name.endsWith('.csv'))) {
+// 		alert('CSV file has been selected');
+// 		handleCsvFile(csvFile);
+// 	} else {
+// 		alert('Please drop a CSV file.');
+// 	}
+// });
+
+// function handleCsvFile(file) {
+// 	console.log(file.name);
+// 	// Implement file handling logic here
+// }
