1f79260570308b587711374f8e731f8fca2af4b1
Marcus <marc3277@edu.zealand.dk>
2024-12-05 16:57:55 +0100

    made convertingDateFormat function, for more cleaniness

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -3,72 +3,45 @@ const userM = require('../models/userModel');
 const { replacePlaceholder } = require('../models/templateModel');
 const async = require('hbs/lib/async');
 
+function convertingDateFormat(date) {
+    return new Date(date).toLocaleDateString('en-GB', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+    }).replaceAll('/', '-').replaceAll(".", "-")
+}
+
 exports.dashboard = async function (req, res) {
 	try {
 		const { stack_name, domain_name, chosen_template } = req.body;
-		// if(!req.session.userDetails) {
-		//      throw new Error("You are not logged in")
-		// }
-		// const jwt = await userM.getJWTfromUser(req.session.userDetails.userId);
+		if(!req.session.userDetails) {
+		     throw new Error("You are not logged in")
+		}
+		const jwt = await userM.getJWTfromUser(req.session.userDetails.userId);
 		let testo;
-		//console.log(await dashboardM.portainerSystemAuth())
+		// console.log(await dashboardM.portainerSystemAuth())
 		//await dashboardM.portainerSystemInfo("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywidXNlcm5hbWUiOiJjb2RlY3JldyIsInJvbGUiOjIsInNjb3BlIjoiZGVmYXVsdCIsImZvcmNlQ2hhbmdlUGFzc3dvcmQiOmZhbHNlLCJleHAiOjE3MzIxMzYzNDMsImlhdCI6MTczMjEwNzU0MywianRpIjoiYzdhMGZlZGQtYTQ5Yy00YTA2LTllNTItYTU5YWRkMzZhNTNiIn0.tM-Bi6y7EBUagHcFRQ60FrHiT3amCGAtcvtikP5evno")
 		//await dashboardM.portainerSystemStatus("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywidXNlcm5hbWUiOiJjb2RlY3JldyIsInJvbGUiOjIsInNjb3BlIjoiZGVmYXVsdCIsImZvcmNlQ2hhbmdlUGFzc3dvcmQiOmZhbHNlLCJleHAiOjE3MzIxMzYzNDMsImlhdCI6MTczMjEwNzU0MywianRpIjoiYzdhMGZlZGQtYTQ5Yy00YTA2LTllNTItYTU5YWRkMzZhNTNiIn0.tM-Bi6y7EBUagHcFRQ60FrHiT3amCGAtcvtikP5evno")
-		const stacks = await dashboardM.portainerStacks(
-			'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywidXNlcm5hbWUiOiJjb2RlY3JldyIsInJvbGUiOjIsInNjb3BlIjoiZGVmYXVsdCIsImZvcmNlQ2hhbmdlUGFzc3dvcmQiOmZhbHNlLCJleHAiOjE3MzMzNzIyMTQsImlhdCI6MTczMzM0MzQxNCwianRpIjoiZmE4MDRmN2QtODg4NC00MzQ0LTk4ZTMtNTE0Njk0OGU5OGE5In0.fpGISoy0T48zc1kqDJgjiVNccjzPxw9AaCugQssdEyM'
-		);
+		const stacks = await dashboardM.portainerStacks('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywidXNlcm5hbWUiOiJjb2RlY3JldyIsInJvbGUiOjIsInNjb3BlIjoiZGVmYXVsdCIsImZvcmNlQ2hhbmdlUGFzc3dvcmQiOmZhbHNlLCJleHAiOjE3MzM0NDI2OTUsImlhdCI6MTczMzQxMzg5NSwianRpIjoiNGI1M2M3YjQtZjU3ZS00OGJjLTk1NDMtOTU0OGQ2MjBiZTQ4In0.CWvNwUT8lbaCYUu9qUnxv7xLMg4piHxGQuiQs6eagSY');
 		const reeeeee = [];
 
 		for (let i = 0; i < stacks.length; i++) {
 			reeeeee.push({
-				userId: req.session.userDetails || 8,
+				userId: req.session.userDetails.userId || 8,
 				name: stacks[i].Name,
 				status: stacks[i].Status === 1,
-				creationDate: new Date(stacks[i].CreationDate * 1000)
-					.toLocaleDateString('en-GB', {
-						day: '2-digit',
-						month: '2-digit',
-						year: 'numeric',
-						hour: '2-digit',
-						minute: '2-digit',
-						second: '2-digit',
-					})
-					.replaceAll('/', '-'),
+				creationDate: convertingDateFormat(stacks[i].CreationDate * 1000),
 				lastUpdate:
 					stacks[i].UpdateDate == 0
-						? new Date(stacks[i].CreationDate * 1000)
-								.toLocaleDateString('en-GB', {
-									day: '2-digit',
-									month: '2-digit',
-									year: 'numeric',
-									hour: '2-digit',
-									minute: '2-digit',
-									second: '2-digit',
-								})
-								.replaceAll('/', '-')
-						: new Date(stacks[i].UpdateDate * 1000)
-								.toLocaleDateString('en-GB', {
-									day: '2-digit',
-									month: '2-digit',
-									year: 'numeric',
-									hour: '2-digit',
-									minute: '2-digit',
-									second: '2-digit',
-								})
-								.replaceAll('/', '-'),
+						? convertingDateFormat(stacks[i].CreationDate * 1000)
+						: convertingDateFormat(stacks[i].UpdateDate * 1000),
 				createdBy: stacks[i].CreatedBy,
 				template: stacks[i].EntryPoint,
 				subDomain: 'ehhh, brain no work',
-				lastActive: new Date()
-					.toLocaleDateString('en-GB', {
-						day: '2-digit',
-						month: '2-digit',
-						year: 'numeric',
-						hour: '2-digit',
-						minute: '2-digit',
-						second: '2-digit',
-					})
-					.replaceAll('/', '-'),
+				lastActive: convertingDateFormat(new Date()),
 				author: 'welp', // firstname lastname function based on userId
 			});
 			// filter reeeee baseret på userId
@@ -80,9 +53,7 @@ exports.dashboard = async function (req, res) {
 			stack: reeeeee,
 		};
 
-		console.log('welp');
-
-		console.log(testo);
+		//console.log(testo);
 		//await dashboardM.portainerEndpoints("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywidXNlcm5hbWUiOiJjb2RlY3JldyIsInJvbGUiOjIsInNjb3BlIjoiZGVmYXVsdCIsImZvcmNlQ2hhbmdlUGFzc3dvcmQiOmZhbHNlLCJleHAiOjE3MzIxMzYzNDMsImlhdCI6MTczMjEwNzU0MywianRpIjoiYzdhMGZlZGQtYTQ5Yy00YTA2LTllNTItYTU5YWRkMzZhNTNiIn0.tM-Bi6y7EBUagHcFRQ60FrHiT3amCGAtcvtikP5evno")
 		//console.log(await replacePlaceholder(4, 'domain_name'));
 
