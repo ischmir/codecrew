35e60da0682d5cdeba61ab90072094ded74ffc61
Mai Jockwich <mmjockwich@gmail.com>
2024-12-10 21:44:12 +0100

    Opdateret team table, og fixet test data
    Jeg har ændret 3 til 1 eller 2 da der kun var 2 test brugere

--- DB.sql
+++ DB.sql
@@ -30,7 +30,9 @@ CREATE TABLE Roles (
 CREATE TABLE Teams (
     teamId int AUTO_INCREMENT PRIMARY KEY,
     teamName varchar(100),
-    teamCreationDate Date
+    teamCreationDate Date,
+    teamDescription varchar(255),
+    teamExpireDate Date
 );
 
 CREATE TABLE Users (
@@ -72,7 +74,7 @@ CREATE table Stacks (
 -- -------------------------
 -- INSERT DATA
 
-INSERT INTO Options (optionsName, optionValue, optionsLastUpdate) VALUES
+INSERT INTO Options (optionsName, optionsValue, optionsLastUpdate) VALUES
     ('apiToken1', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxMjM0NTY3ODkwIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c', '2024-10-01 10:00:00'),
     ('apiToken2', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.S4fK9wRUSMeKKF2QT4fwpMeJf6POk6yJV_cQhtTsw5b', '2024-10-05 14:30:00'),
     ('authToken', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJjb21wYW55LmNvbSIsInVzZXJJZCI6NDM2NzI1fQ.s7fLyRSMeKKF2QT4fwpMeJf36POk6yJWf_dQYskkFzq', '2024-10-07 09:45:00'),
@@ -168,11 +170,12 @@ INSERT INTO Roles (roleName, stackLimit, accessLevel) VALUES
     ('SuperAdmin', 9999, 'superAdmin');
 
 -- TEAMS
-INSERT INTO Teams (teamName, teamCreationDate) VALUES 
-    ('No Team', '2000-01-01'),
-    ('Development Team', '2024-10-01'),
-    ('Design Team', '2024-10-05'),
-    ('QA Team', '2024-10-10');
+INSERT INTO Teams (teamName, teamCreationDate,teamDescription,teamExpireDate) VALUES 
+    ('No Team', '2000-01-01', 'team tech', '2001-01-01'),
+    ('Development Team', '2024-10-01','Team Deeppink','2025-10-01'),
+    ('Design Team', '2024-10-05', 'Selfies foran Biltema!!', '2025-10-05'),
+    ('QA Team', '2024-10-10', 'The test team!!', '2025-10-10' );
+    
     
 -- Users
 INSERT INTO Users (username, firstName, lastName, userEmail, userPassword, userProfilePicture, userExpirationDate, FK_role, FK_options) VALUES
@@ -209,15 +212,15 @@ INSERT INTO Stacks (subDomain, FK_templateId, FK_userId, stackName, stackCreatio
 VALUES
     ('kenneth.dev', 1, 1, 'Kenneth Stack 1', '2024-01-10', '2024-02-15 12:00:00', '2024-03-01 14:30:00'),
     ('kenneth.prod', 2, 1, 'Kenneth Stack 2', '2024-02-05', '2024-03-18 09:20:00', '2024-04-12 17:45:00'),
-    ('kenneth.test', 3, 1, 'Kenneth Stack 3', '2024-03-07', '2024-03-30 11:10:00', '2024-05-06 10:50:00'),
+    ('kenneth.test', 2, 1, 'Kenneth Stack 3', '2024-03-07', '2024-03-30 11:10:00', '2024-05-06 10:50:00'),
     
     ('bob.dev', 2, 2, 'Bob Stack 1', '2024-02-20', '2024-03-22 10:15:00', '2024-03-25 16:00:00'),
-    ('charlie.dev', 1, 3, 'Charlie Stack', '2024-01-25', '2024-02-10 09:00:00', '2024-02-15 11:45:00'),
+    ('charlie.dev', 1, 2, 'Charlie Stack', '2024-01-25', '2024-02-10 09:00:00', '2024-02-15 11:45:00'),
     
-    ('diana.dev', 3, 4, 'Diana Stack', '2024-02-15', '2024-03-05 13:30:00', '2024-03-10 10:20:00'),
+    ('diana.dev', 1, 4, 'Diana Stack', '2024-02-15', '2024-03-05 13:30:00', '2024-03-10 10:20:00'),
     
     ('fiona.prod', 1, 6, 'Fiona Stack', '2024-03-01', '2024-03-18 08:25:00', '2024-04-01 14:40:00'),
     ('george.test', 2, 7, 'George Stack', '2024-02-22', '2024-03-01 15:10:00', '2024-03-29 09:35:00'),
     
     ('hannah.dev', 1, 8, 'Hannah Stack', '2024-04-04', '2024-04-10 10:00:00', '2024-04-11 15:00:00'),
-    ('jessica.prod', 3, 10, 'Jessica Stack', '2024-02-28', '2024-03-12 17:05:00', '2024-04-05 18:25:00');
+    ('jessica.prod', 2, 10, 'Jessica Stack', '2024-02-28', '2024-03-12 17:05:00', '2024-04-05 18:25:00');
