c1f909cbeef952debb97e0d65f3ab723768626a4
Mai Jockwich <mmjockwich@gmail.com>
2024-12-10 21:08:14 +0100

    Funktioner til at oprette team og gemme teamet i dblavet

--- controllers/teamsController.js
+++ controllers/teamsController.js
@@ -1,16 +1,22 @@
-const teamsM = require('../models/teamsModel');
+const teamsModel = require('../models/teamsModel');
 
-exports.teams = function (req, res) {
-	res.render('teams-create', teamsM.teams());
+exports.teams = function (requst, respons) {
+	respons.render('teams-create', teamsModel.teams());
 };
 
-exports.teamsEdit = function (req, res) {
-	res.render('teams-edit', teamsM.teams());
+exports.teamsEdit = function (requst, respons) {
+	respons.render('teams-edit', teamsModel.teams());
 };
-
+// Request.body =
+// name: 'Howdy Team',
+// description: 'Yeeeehaw!!!!!        ',
+// expire: '2024-12-28'
 exports.postNewTeam = async function (requst, respons) {
 	//Lav tjek om man har adgang til at oprette team
 	// Gemme daten i databasen
+	await teamsModel.addTeamToDB(requst.body);
+	console.log(teamsModel);
 	// Redirect til Edit exsisting team
-	console.log(requst.body);
+	respons.redirect('/teams-edit');
+	// console.log(requst.body);
 };
--- models/teamsModel.js
+++ models/teamsModel.js
@@ -1,7 +1,15 @@
+const db = require('../config/db');
 exports.teams = () => {
 	const data = {
 		title: 'Teams',
 	};
 
 	return data;
+};
+// team =
+// name: 'Howdy Team',
+// description: 'Yeeeehaw!!!!!        ',
+// expire: '2024-12-28'
+exports.addTeamToDB = async function (team) {
+	await db.query('INSERT INTO Teams (teamName, teamCreationDate) VALUES (?, NOW());', [team.name]);
 };
