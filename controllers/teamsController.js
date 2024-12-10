1b21e9f1935a16b22e41384bcc79a128568ac14f
Mai Jockwich <mmjockwich@gmail.com>
2024-12-10 20:00:00 +0100

    Create team virker, men mangler så det gemmes på databasen. NEXT!
    pyyyh jeg er træt i hovdet nu :')

--- controllers/teamsController.js
+++ controllers/teamsController.js
@@ -7,3 +7,10 @@ exports.teams = function (req, res) {
 exports.teamsEdit = function (req, res) {
 	res.render('teams-edit', teamsM.teams());
 };
+
+exports.postNewTeam = async function (requst, respons) {
+	//Lav tjek om man har adgang til at oprette team
+	// Gemme daten i databasen
+	// Redirect til Edit exsisting team
+	console.log(requst.body);
+};
--- routes/postSiteRoutes.js
+++ routes/postSiteRoutes.js
@@ -1,16 +1,18 @@
-const loginC = require("../controllers/loginController");
-const settingC = require("../controllers/settingController");
-const templateC = require("../controllers/templateController");
-const stackC = require("../controllers/dashboardController");
+const loginC = require('../controllers/loginController');
+const settingC = require('../controllers/settingController');
+const templateC = require('../controllers/templateController');
+const stackC = require('../controllers/dashboardController');
+const teamController = require('../controllers/teamsController');
 
-module.exports = function(app) {
-    app.post("/login", loginC.postLogin, loginC.sendJWTtoUser);
-    app.post("/settings-password", settingC.updatePassword);
-    app.post("/upgradeUser", settingC.upgradeUser);
-    app.post("/updateTemplate", templateC.updateTemplate);
-    app.post("/templateCreation", templateC.templateCreation);
-    app.post("/deleteTemplate", templateC.deleteTemplate);
-    app.post("/createStack", stackC.createStack);
-    app.post("/startStack", stackC.startStack);
-    app.post("/stopStack", stackC.stopStack);
-}
+module.exports = function (app) {
+	app.post('/login', loginC.postLogin, loginC.sendJWTtoUser);
+	app.post('/settings-password', settingC.updatePassword);
+	app.post('/upgradeUser', settingC.upgradeUser);
+	app.post('/updateTemplate', templateC.updateTemplate);
+	app.post('/templateCreation', templateC.templateCreation);
+	app.post('/deleteTemplate', templateC.deleteTemplate);
+	app.post('/createStack', stackC.createStack);
+	app.post('/startStack', stackC.startStack);
+	app.post('/stopStack', stackC.stopStack);
+	app.post('/createTeam', teamController.postNewTeam);
+};
