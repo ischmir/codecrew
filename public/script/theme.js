7c19fe4f6200f3d2159c8060657be9178177a977
Mai Jockwich <mmjockwich@gmail.com>
2024-12-18 22:44:56 +0100

    Small changes :**

--- public/script/theme.js
+++ public/script/theme.js
@@ -1,16 +1,4 @@
-//lytter efter click
-// document.getElementById('theme-toggle').addEventListener('click', function () {
-// 	//henter attribute data-Theme
-// 	const currentTheme = document.documentElement.getAttribute('data-theme');
-// 	// tjekker hvis det er dark så skal det var ligth og omvent
-// 	const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
-// 	//sætter det nye theme udfra overstående
-// 	document.documentElement.setAttribute('data-theme', newTheme);
-
-// 	// Gem det valgte tema i localStorage, så det huskes
-// 	localStorage.setItem('theme', newTheme);
-// });
-console.log('Hello :)');
+// console.log('Hello :)');
 // Tjek ved load hvilket tema brugeren sidst valgte
 const savedTheme = localStorage.getItem('theme') || 'light';
 document.documentElement.setAttribute('data-theme', savedTheme);
