32cbed15299fa876b19c2c54822dae7688da9413
Mai Jockwich <mmjockwich@gmail.com>
2024-12-20 00:39:11 +0100

    Ændret logoet så det er tilpasset darkmode lightmode

--- controllers/teamsController.js
+++ controllers/teamsController.js
@@ -12,7 +12,6 @@ exports.teamsEdit = async function (requst, respons) {
 // description: 'Yeeeehaw!!!!!        ',
 // expire: '2024-12-28'
 exports.postNewTeam = async function (requst, respons) {
-	//Lav tjek om man har adgang til at oprette team
 	// Gemme daten i databasen
 	const teamId = await teamsModel.addTeamToDB(requst.body);
 	// Redirect til Edit exsisting team
@@ -20,7 +19,6 @@ exports.postNewTeam = async function (requst, respons) {
 	// console.log(requst.body);
 };
 exports.postAddTeamMember = async function (requst, respons) {
-	//Lav tjek om man har adgang til at oprette team
 	// Gemme daten i databasen
 	console.log('Tilføj member', requst.body.member);
 	// await
