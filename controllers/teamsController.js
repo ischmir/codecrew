ad92a0f714d98ae16d199f24c38cf6f40c55a49d
Mai Jockwich <mmjockwich@gmail.com>
2025-01-13 18:39:16 +0100

    Sikkerhed stilladelser

--- controllers/teamsController.js
+++ controllers/teamsController.js
@@ -2,18 +2,31 @@ const teamsModel = require('../models/teamsModel');
 // Håndterer forespørgsler for teams
 
 exports.teams = async function (requst, respons) {
+	if (!requst.session.userDetails.isTeamAllowed) {
+		respons.redirect('/dashboard');
+		return;
+	}
 	// Henter alle teams fra databasen
 	respons.render('teams-create', await teamsModel.teams());
 	// Render teams-create-view med alle teams
 };
 
 exports.teamsEdit = async function (requst, respons) {
+	if (!requst.session.userDetails.isTeamAllowed) {
+		respons.redirect('/dashboard');
+		return;
+	}
+
 	// Render teams-edit-view med et specifikt team
 	respons.render('teams-edit', await teamsModel.teams(requst.params.id));
 	// Render teams-edit-view med et specifikt team
 };
 
 exports.postNewTeam = async function (requst, respons) {
+	if (!requst.session.userDetails.isTeamAllowed) {
+		respons.redirect('/dashboard');
+		return;
+	}
 	// Gemme daten i databasen
 	const teamId = await teamsModel.addTeamToDB(requst.body);
 	// Tilføjer et team til databasen
@@ -21,6 +34,11 @@ exports.postNewTeam = async function (requst, respons) {
 	// Redirect til Edit exsisting team
 };
 exports.postAddTeamMember = async function (requst, respons) {
+	if (!requst.session.userDetails.isTeamAllowed) {
+		respons.redirect('/dashboard');
+		return;
+	}
+
 	// Gemme daten i databasen
 	console.log('Tilføj member', requst.body.member);
 	// Tilføjer et medlem til et team
